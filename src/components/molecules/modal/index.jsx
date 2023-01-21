20079fd5744ddcb4fe3c35741e2230161b93edfd
Bear_bear <unegbuclinton5@gmail.com>
2023-01-21 08:24:39 +0100

    bug fix

--- src/components/molecules/modal/index.jsx
+++ src/components/molecules/modal/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 import { COLORS } from '../../../constants/colors';
-// import { DPIconCancelBtn } from '../../../icons';
 
 const Modal = ({ children, show, hide }) => {
   if (!show) {
