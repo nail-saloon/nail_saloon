ecbfb138eff6cf56bf4865c3e9c4a64b574bc0d6
Bear_bear <unegbuclinton5@gmail.com>
2023-01-28 19:46:48 +0100

    update on fixes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import Structure from '../components/layouts/Structure';
 import Carousel from '../components/molecules/carousel';
 import FocusComponent from '../components/molecules/focusComp';
-import Map from '../components/molecules/googleMap';
+// import Map from '../components/molecules/googleMap';
 import PostHero from '../components/molecules/PostHero/Index';
 import Reviews from '../components/molecules/Reviews';
 
 const Home = () => {
-  const location = {
-    // address: '1600 Amphitheatre Parkway, Mountain View, california.',
-    lat: 37.42216,
-    lng: -122.08427,
-  };
+  // const location = {
+  //   // address: '1600 Amphitheatre Parkway, Mountain View, california.',
+  //   lat: 37.42216,
+  //   lng: -122.08427,
+  // };
 
   return (
     <Structure>
